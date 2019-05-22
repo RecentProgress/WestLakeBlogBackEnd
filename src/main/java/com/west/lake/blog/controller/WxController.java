587506311ba037a11c7ed39f60879471537f5ad2
futao <1185172056@qq.com>
2019-05-22 15:01:01 +0800

    wxToken1

--- src/main/java/com/west/lake/blog/controller/WxController.java
+++ src/main/java/com/west/lake/blog/controller/WxController.java
@@ -1,12 +1,14 @@
 package com.west.lake.blog.controller;
 
-import com.west.lake.blog.tools.RequestTools;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.Charset;
+import java.util.TreeSet;
+
 
 /**
  * @author futao
@@ -16,9 +18,40 @@ import javax.servlet.http.HttpServletRequest;
 @RestController("/wx")
 public class WxController {
 
+    /**
+     * @param echostr   随机字符串
+     * @param signature 微信加密签名，signature结合了开发者填写的token参数和请求中的timestamp参数、nonce参数。
+     * @param timestamp 时间戳
+     * @param nonce     随机数
+     * @return
+     */
     @GetMapping("/token")
-    public String token(HttpServletRequest request) {
-        log.info("收到微信请求:[{}]", RequestTools.queryParameters(request));
-        return "success";
+    public String token(
+            @RequestParam String echostr,
+            @RequestParam String signature,
+            @RequestParam String timestamp,
+            @RequestParam String nonce) {
+
+        /*
+        1）将token、timestamp、nonce三个参数进行字典序排序
+        2）将三个参数字符串拼接成一个字符串进行sha1加密
+        3）开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+         */
+        log.info("接收到微信请求:echostr:[{}],signature:[{}],timestamp:[{}],nonce:[{}]", echostr, signature, timestamp, nonce);
+        TreeSet<String> treeSet = new TreeSet<>();
+        treeSet.add("wxToken");
+        treeSet.add(timestamp);
+        treeSet.add(nonce);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s : treeSet) {
+            stringBuilder.append(s);
+        }
+        String result = Hex.encodeHex(stringBuilder.toString().getBytes(Charset.forName("UTF-8"))).toString();
+        log.info("result:[{}]", result);
+        if (result.equals(signature)) {
+
+        }
+        return echostr;
     }
 }
