c8e6574b23b09d967d61135973d06440315d5833
futao <1185172056@qq.com>
2019-05-22 14:34:00 +0800

    wxToken

--- src/main/java/com/west/lake/blog/controller/WxController.java
+++ src/main/java/com/west/lake/blog/controller/WxController.java
@@ -1,5 +1,6 @@
 package com.west.lake.blog.controller;
 
+import com.west.lake.blog.tools.RequestTools;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,13 +13,12 @@ import javax.servlet.http.HttpServletRequest;
  * Created on 2019-05-22.
  */
 @Slf4j
-@RestController("/")
+@RestController("/wx")
 public class WxController {
 
-    @GetMapping("/")
+    @GetMapping("/token")
     public String token(HttpServletRequest request) {
-        String wxToken = request.getParameter("wxToken");
-        log.info("收到微信请求:[{}]", wxToken);
-        return wxToken;
+        log.info("收到微信请求:[{}]", RequestTools.queryParameters(request));
+        return "success";
     }
 }
